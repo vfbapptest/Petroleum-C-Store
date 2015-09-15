bc11aa47dc313acbec9355d61ad01a198628cf36
vfbapptest <vfbapptest@gmail.com>
2015-09-15 12:35:25 -0400

    Initial Commit

---
+++ Petroleum-C-Store/src/petroleumCStore/PetroleumClass.java
@@ -0,0 +1,10 @@
+package petroleumCStore;
+
+public class PetroleumClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Petroleum & C-Store software project");
+
+	}
+
+}
